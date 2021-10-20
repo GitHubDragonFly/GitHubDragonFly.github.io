4297293b162058b6a30e75511988657ea1c50134
GitHubDragonFly <69928501+GitHubDragonFly@users.noreply.github.com>
2021-10-20 14:46:39 -0400

    Add files via upload

---
+++ viewers/static/jsm/extensions/loaders/KHR_materials_variants/KHR_materials_variants.js
@@ -0,0 +1,273 @@
+/**
+ * Materials variants extension
+ *
+ * Specification: https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_materials_variants
+ */
+
+/**
+ * KHR_materials_variants specification allows duplicated variant names
+ * but it makes handling the extension complex.
+ * We ensure tha names and make it easier.
+ * If you want to export the extension with the original names
+ * you are recommended to write GLTFExporter plugin to restore the names.
+ *
+ * @param variantNames {Array<string>}
+ * @return {Array<string>}
+ */
+const ensureUniqueNames = (variantNames) => {
+  const uniqueNames = [];
+  const knownNames = new Set();
+
+  for (const name of variantNames) {
+    let uniqueName = name;
+    let suffix = 0;
+    // @TODO: An easy solution.
+    //        O(N^2) in the worst scenario where N is variantNames.length.
+    //        Fix me if needed.
+    while (knownNames.has(uniqueName)) {
+      uniqueName = name + '.' + (++suffix);
+    }
+    knownNames.add(uniqueName);
+    uniqueNames.push(uniqueName);
+  }
+
+  return uniqueNames;
+};
+
+/**
+ * Convert mappings array to table object to make handling the extension easier.
+ *
+ * @param extensionDef {glTF.meshes[n].primitive.extensions.KHR_materials_variants}
+ * @param variantNames {Array<string>} Required to be unique names
+ * @return {Object}
+ */
+const mappingsArrayToTable = (extensionDef, variantNames) => {
+  const table = {};
+  for (const mapping of extensionDef.mappings) {
+    for (const variant of mapping.variants) {
+      table[variantNames[variant]] = {
+        material: null,
+        gltfMaterialIndex: mapping.material
+      };
+    }
+  }
+  return table;
+};
+
+/**
+ * @param object {THREE.Object3D}
+ * @return {boolean}
+ */
+const compatibleObject = object => {
+  return object.material !== undefined && // easier than (!object.isMesh && !object.isLine && !object.isPoints)
+    object.userData && // just in case
+    object.userData.variantMaterials;
+};
+
+/**
+ * @param obj1 {THREE.Object3D}
+ * @param obj2 {THREE.Object3D}
+ * @param callback {function}
+ */
+const traversePair = (obj1, obj2, callback) => {
+  callback(obj1, obj2);
+  // Assume obj1 and obj2 have the same tree structure
+  for (let i = 0; i < obj1.children.length; i++) {
+    traversePair(obj1.children[i], obj2.children[i], callback);
+  }
+};
+
+// Variant materials and original material instances are stored under
+// object.userData.variantMaterials/originalMaterial.
+// Three.js Object3D.cppy/clone() doesn't copy/clone Three.js objects under
+// .userData so this function is a workaround.
+/**
+ * @param dst {THREE.Object3D}
+ * @param src {THREE.Object3D}
+ * @param callback {function}
+ */
+const copyVariantMaterials = (dst, src) => {
+  if (src.userData.variantMaterials !== undefined) {
+    dst.userData.variantMaterials = Object.assign({}, src.userData.variantMaterials);
+  }
+  if (src.userData.originalMaterial !== undefined) {
+    dst.userData.originalMaterial = src.userData.originalMaterial;
+  }
+};
+
+export default class GLTFMaterialsVariantsExtension {
+  constructor(parser) {
+    this.parser = parser;
+    this.name = 'KHR_materials_variants';
+  }
+
+  // Note that the following properties will be overridden even if they are pre-defined
+  // - gltf.userData.variants
+  // - mesh.userData.variantMaterials
+  afterRoot(gltf) {
+    const parser = this.parser;
+    const json = parser.json;
+
+    if (!json.extensions || !json.extensions[this.name]) {
+      return null;
+    }
+
+    const extensionDef = json.extensions[this.name];
+    const variantsDef = extensionDef.variants || [];
+    const variants = ensureUniqueNames(variantsDef.map(v => v.name));
+
+    for (const scene of gltf.scenes) {
+      // Save the variants data under associated mesh.userData
+      scene.traverse(object => {
+        // The following code can be simplified if parser.associations directly supports meshes.
+        const association = parser.associations.get(object);
+
+        if (!association || association.type !== 'nodes') {
+          return;
+        }
+
+        const nodeDef = json.nodes[association.index];
+        const meshIndex = nodeDef.mesh;
+
+        if (meshIndex === undefined) {
+          return;
+        }
+
+        // Two limitations:
+        // 1. The nodeDef shouldn't have any objects (camera, light, or nodeDef.extensions object)
+        //    other than nodeDef.mesh
+        // 2. Other plugins shouldn't change any scene graph hierarchy
+        // The following code can cause error if hitting the either or both limitations
+        // If parser.associations will directly supports meshes these limitations can be removed
+
+        const meshDef = json.meshes[meshIndex];
+        const primitivesDef = meshDef.primitives;
+        const meshes = object.isMesh ? [object] : object.children;
+
+        for (let i = 0; i < primitivesDef.length; i++) {
+          const primitiveDef = primitivesDef[i];
+          const extensionsDef = primitiveDef.extensions;
+          if (!extensionsDef || !extensionsDef[this.name]) {
+            continue;
+          }
+          meshes[i].userData.variantMaterials = mappingsArrayToTable(extensionsDef[this.name], variants);
+        }
+      });
+    }
+
+    gltf.userData.variants = variants;
+
+    // @TODO: Adding new unofficial property .functions.
+    //        It can be problematic especially with TypeScript?
+    gltf.functions = gltf.functions || {};
+
+    /**
+     * @param object {THREE.Mesh}
+     * @param variantName {string|null}
+     * @return {Promise}
+     */
+    const switchMaterial = async (object, variantName, onUpdate) => {
+      if (!object.userData.originalMaterial) {
+        object.userData.originalMaterial = object.material;
+      }
+
+      const oldMaterial = object.material;
+      let gltfMaterialIndex = null;
+
+      if (variantName === null || !object.userData.variantMaterials[variantName]) {
+        object.material = object.userData.originalMaterial;
+        if (parser.associations.has(object.material)) {
+          gltfMaterialIndex = parser.associations.get(object.material).index;
+        }
+      } else {
+        const variantMaterialParam = object.userData.variantMaterials[variantName];
+
+        if (variantMaterialParam.material) {
+          object.material = variantMaterialParam.material;
+          if (variantMaterialParam.gltfMaterialIndex !== undefined) {
+            gltfMaterialIndex = variantMaterialParam.gltfMaterialIndex;
+          }
+        } else {
+          // Assume the variant material is defined in glTF
+          gltfMaterialIndex = variantMaterialParam.gltfMaterialIndex;
+          object.material = await parser.getDependency('material', gltfMaterialIndex);
+          parser.assignFinalMaterial(object);
+          variantMaterialParam.material = object.material;
+        }
+      }
+
+      if (onUpdate !== null) {
+        onUpdate(object, oldMaterial, gltfMaterialIndex);
+      }
+    };
+
+    /**
+     * @param object {THREE.Mesh}
+     * @return {Promise}
+     */
+    const ensureLoadVariants = object => {
+      const currentMaterial = object.material;
+      const variantMaterials = object.userData.variantMaterials;
+      const pending = [];
+      for (const variantName in variantMaterials) {
+        const variantMaterial = variantMaterials[variantName];
+        if (variantMaterial.material) {
+          continue;
+        }
+        const materialIndex = variantMaterial.gltfMaterialIndex;
+        pending.push(parser.getDependency('material', materialIndex).then(material => {
+          object.material = material;
+          parser.assignFinalMaterial(object);
+          variantMaterials[variantName].material = object.material;
+        }));
+      }
+      return Promise.all(pending).then(() => {
+        object.material = currentMaterial;
+      });
+    };
+
+    /**
+     * @param object {THREE.Object3D}
+     * @param variantName {string|null}
+     * @param doTraverse {boolean} Default is true
+     * @return {Promise}
+     */
+    gltf.functions.selectVariant = (object, variantName, doTraverse = true, onUpdate = null) => {
+      const pending = [];
+      if (doTraverse) {
+        object.traverse(o => compatibleObject(o) && pending.push(switchMaterial(o, variantName, onUpdate)));
+      } else {
+        compatibleObject(object) && pending.push(switchMaterial(object, variantName, onUpdate));
+      }
+      return Promise.all(pending);
+    };
+
+    /**
+     * @param object {THREE.Object3D}
+     * @param doTraverse {boolean} Default is true
+     * @return {Promise}
+     */
+    gltf.functions.ensureLoadVariants = (object, doTraverse = true) => {
+      const pending = [];
+      if (doTraverse) {
+        object.traverse(o => compatibleObject(o) && pending.push(ensureLoadVariants(o)));
+      } else {
+        compatibleObject(object) && pending.push(ensureLoadVariants(object));
+      }
+      return Promise.all(pending);
+    };
+
+    /**
+     * @param dst {THREE.Object3D}
+     * @param src {THREE.Object3D}
+     * @param doTraverse {boolean} Default is true
+     */
+    gltf.functions.copyVariantMaterials = (dst, src, doTraverse = true) => {
+      if (doTraverse) {
+        traversePair(dst, src, (dst, src) => copyVariantMaterials(dst, src));
+      } else {
+        copyVariantMaterials(dst, src);
+      }
+    };
+  }
+}
---
+++ viewers/static/jsm/extensions/loaders/KHR_materials_variants/README.md
@@ -0,0 +1,100 @@
+# [Three.js](https://threejs.org) [GLTFLoader](https://threejs.org/docs/#examples/en/loaders/GLTFLoader) [KHR_materials_variants](https://github.com/KhronosGroup/glTF/tree/master/extensions/2.0/Khronos/KHR_materials_variants) extension
+
+## How to use
+
+```javascript
+import * as THREE from 'path_to_three.module.js';
+import {GLTFLoader} from 'path_to_GLTFLoader.js';
+import GLTFMaterialsVariantsExtension from 'path_to_three-gltf-extensions/loaders/KHR_materials_variants/KHR_materials_variants.js';
+
+const loader = new GLTFLoader();
+loader.register(parser => new GLTFMaterialsVariantsExtension(parser));
+loader.load(path_to_gltf_asset, async gltf => {
+  scene.add(gltf.scene);
+  const variants = gltf.userData.variants; // [variantName0, variantName1, ...]
+  const variantName = variants[theIndexYouWantToUse];
+  await gltf.functions.selectVariant(gltf.scene, variantName);
+  render();
+});
+```
+
+## Compatible Three.js revision
+
+&gt;= r126
+
+## API
+
+If glTF file includes `KHR_materials_variants` extension, the result returned by the loader will have the following data and function.
+
+**gltf.userData.variants**
+
+`variants` is an array of variant names like `[variantName0, variantName1, ...]`.
+
+**mesh.userData.variantMaterials**
+
+```javascript
+{
+  variantName0: {
+    material: null,
+    gltfMaterialIndex: number
+  },
+  variantName1: {
+    material: null,
+    gltfMaterialIndex: number
+  },
+  ...
+}
+```
+
+`variantMaterials` is an object whose key is variant name and whose value has an index to glTF material index and variant material instance cache.
+Variant material cache is saved in `selectVariant()` or `ensureLoadVariants()`.
+The cache is also used when exporting variant materials.
+
+**gltf.functions.selectVariant(object: THREE.Object3D, variantName: string | null, doTraverse = true: boolean, onLoad = null: function | null): Promise**
+
+`selectVariant()` is a function to switch materials to the ones associated with `variantName`.
+Unless `doTraverse` is set to `false` the function traverses the children and applys the change to all the child objects, too.
+If a variant material associated with a passed variant name is undefined for a mesh or variant name is null, the function selects an original material.
+You can pass `onUpdate()` callback function whici is fired with Three.js object, old material, and gltf material index
+(`null` if the new material is not found in glTF file) when a material of an object is switched.
+The returned Promise will be resolved when all the selected materials are ready.
+
+**gltf.functions.ensureLoadVariants(object: THREE.Object3D, doTraverse = true: boolean): Promise**
+
+`ensureLoadVariants()` is a function to ensure all the variant materials are loaded and saved under `mesh.userData.variantMaterials` (See [Side effects](#Side-effects) below).
+Unless `doTraverse` is set to `false`, the function traverses the children and applys it to all the child objects, too.
+You are recommended to call this function before exporting objects with [GLTFExporter KHR_materials_variants plugin](../../exporters/KHR_materials_variants/#README.md)
+
+**gltf.functions.copyVariantMaterials(dst: THREE.Object3D, src: THREE.Object3D, doTraverse = true: boolean): void**
+
+Variant material instances are stored under `.userData` but Three.js `copy/clone()` doesn't copy/clone the Three.js objects　under `.userData`.
+Users need to copy/clone variant materials with this helper function like
+
+```
+const obj2 = obj1.clone();
+gltf.functions.copyVariantMaterials(obj2, obj1);
+```
+
+## Side effects
+
+* `gltf.functions.selectVariant()` saves an original (core-spec) material as `mesh.userData.originalMaterial` for each Mesh
+* `gltf.functions.selectVariant()` saves a selected variant material as `mesh.userData.variantMaterials[variantName].material` for each Mesh. It is expected to be used when exporting with [GLTFExporter KHR_materials_variants plugin](../../exporters/KHR_materials_variants/#README.md).
+* The plugin ensures unique name in variant names so if duplicated names exist the plugin renames them. If you want to export the extension with the original names you are required to write an exporter plugin restoring the names.
+
+## Replace with or add your custom variant materials
+
+You can replace with or add your custom variant materials in your application by setting your custom material instance to `mesh.userData.variantMaterials[variantName].material` like
+
+```
+mesh.userData.variantMaterials[customVariantName] = {material: customVariantMaterial};
+```
+
+You can omit `.gltfMaterialIndex` property and `customVariantName` doesn't have to be in `gltf.userData.variants`.
+
+## Limitations
+
+* `selectVariant()` doesn't have effect to meshes which are already removed from a scene
+* This plugin may not work if a glTF node has camera, light, or other extension objects in addition to mesh. This limitation may be removed if [this suggestion](https://github.com/mrdoob/three.js/pull/19359#issuecomment-774487100) is accepted.
+* `mesh.userData.variantMaterials` are not serialized by Three.js `toJSON()` method correctly because it doesn't support the serialization of Three.js objects under `.userData`.
+* Three.js `copy/clone()` doesn't copy/clone variant and original mateiral instances under `.userData`. You need to use `copyVariantMaterials()` helper function.
+* If you want to manually assign an active material, you also need to update the corresponding property in `.userData`.
