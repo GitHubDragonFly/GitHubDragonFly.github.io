14bc32af24750296906e476e182907710f852cb3
GitHubDragonFly <69928501+GitHubDragonFly@users.noreply.github.com>
2022-04-03 19:51:22 -0400

    Modified OBJ exporter to support MTL exporting
    - Not necessarily accurate but functional

--- viewers/static/js/exporters/OBJExporter.js
+++ viewers/static/js/exporters/OBJExporter.js
@@ -8,6 +8,7 @@
 			let indexVertex = 0;
 			let indexVertexUvs = 0;
 			let indexNormals = 0;
+			let materials = {};
 			const vertex = new THREE.Vector3();
 			const color = new THREE.Color();
 			const normal = new THREE.Vector3();
@@ -39,6 +40,7 @@
 				if ( mesh.material && mesh.material.name ) {
 
 					output += 'usemtl ' + mesh.material.name + '\n';
+					materials[ mesh.material.id ] = mesh.material;
 
 				} // vertices
 
@@ -268,7 +270,107 @@
 				}
 
 			} );
-			return output;
+
+			if (Object.keys( materials ).length !== 0) {
+				// mtl output (Ref: https://stackoverflow.com/questions/35070048/export-a-three-js-textured-model-to-a-obj-with-mtl-file)
+      
+				let mtlOutput = '';
+				let textures = [];
+				const ext = 'png';
+      
+				Object.keys( materials ).forEach( ( key ) => {
+
+					let mat = materials[ key ];
+					let name = ( mat.name ? ( mat.name.toUpperCase().endsWith( '.PNG' ) ? mat.name.substring(0, mat.name.lastIndexOf( '.' ) ) : mat.name ) : mat.name ) || 'material' + mat.id;
+
+					mtlOutput += 'newmtl ' + name + '\n';
+
+					mtlOutput += 'Ns 15.0000\n';
+					mtlOutput += 'Ni 1.5000\n';
+					mtlOutput += 'd 1.0000\n';
+					mtlOutput += 'Tr 0.0000\n';
+					mtlOutput += 'Tf 1.0000 1.0000 1.0000\n';
+					mtlOutput += 'illum 2\n';
+					mtlOutput += 'Ka ' + mat.color.r + ' ' + mat.color.g + ' ' + mat.color.b + ' ' + '\n';
+					mtlOutput += 'Kd ' + mat.color.r + ' ' + mat.color.g + ' ' + mat.color.b + ' ' + '\n';
+					mtlOutput += 'Ks 0.2500 0.2500 0.2500\n';
+					mtlOutput += 'Ke 0.0500 0.0500 0.0500\n';
+
+					if (mat.map && mat.map instanceof THREE.Texture) {
+						textures.push( {
+							name,
+							ext,
+							data: imageToData( mat.map.image, ext )
+						});
+	
+						mtlOutput += 'map_Ka ' + name + '.png' + '\n';
+						mtlOutput += 'map_Kd ' + name + '.png' + '\n';
+
+					}
+
+				});
+
+				return { obj: output, mtl: mtlOutput, tex: textures };
+
+			} else {
+
+				return { obj: output };
+
+			}
+
+			// the following functions were adopted from ColladaExporter.js
+
+			function imageToData( image, ext ) {
+
+				let canvas, ctx;
+
+				canvas = canvas || document.createElement( 'canvas' );
+				ctx = ctx || canvas.getContext( '2d' );
+				canvas.width = image.width;
+				canvas.height = image.height;
+
+				// this seems to work fine for exporting TGA images as PNG but unflipped
+				if ( image.data && image.data.constructor === Uint8Array ) {
+
+					let imgData = ctx.createImageData( image.width, image.height );
+
+					for (let i = 0; i < imgData.data.length; i += 4) {
+
+						imgData.data[ i + 0 ] = image.data[ i + 0 ];
+					  	imgData.data[ i + 1 ] = image.data[ i + 1 ];
+					  	imgData.data[ i + 2 ] = image.data[ i + 2 ];
+					  	imgData.data[ i + 3 ] = image.data[ i + 3 ];
+
+					}
+
+					ctx.putImageData( imgData, 0, 0 );
+
+				} else {
+
+					ctx.drawImage( image, 0, 0 ); // Get the base64 encoded data
+
+				}
+
+				const base64data = canvas.toDataURL( `image/${ext}`, 1 ).replace( /^data:image\/(png|jpg);base64,/, '' ); // Convert to a uint8 array
+
+				return base64ToBuffer( base64data );
+
+			}
+
+			function base64ToBuffer( str ) {
+
+				const b = atob( str );
+				const buf = new Uint8Array( b.length );
+
+				for ( let i = 0, l = buf.length; i < l; i ++ ) {
+
+					buf[ i ] = b.charCodeAt( i );
+
+				}
+
+				return buf;
+
+			}
 
 		}
 
