c61a04f18663934c7aeda8ae7783d732ba2f8be6
GitHubDragonFly <69928501+GitHubDragonFly@users.noreply.github.com>
2023-07-12 18:00:17 -0400

    Update GLTF Loader

--- viewers/static/js/GLTFLoader.js
+++ viewers/static/js/GLTFLoader.js
@@ -3121,7 +3121,7 @@ class GLTFMeshQuantizationExtension {
 	
 				} else if ( source.uri && source.uri.startsWith( '.\\' ) === true && options.resourcePath !== '' ) {
 	
-					source.uri = options.resourcePath + source.uri.substring( source.uri.lastIndexOf( '/' ) + 1 );
+					source.uri = options.resourcePath + source.uri.substring( source.uri.lastIndexOf( '\\' ) + 1 );
 					( source.name === undefined ) ? source[ 'name' ] = source.uri.substring( source.uri.lastIndexOf( '\\' ) + 1 ) : source.name = source.uri.substring( source.uri.lastIndexOf( '\\' ) + 1 );
 	
 				} else if ( source.uri && options.resourcePath !== '' && source.uri.startsWith( options.resourcePath ) === false ) {
