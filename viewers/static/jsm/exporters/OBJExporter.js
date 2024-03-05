fffe8e70d4c348672955852c2c2c963176127d4f
GitHubDragonFly <69928501+GitHubDragonFly@users.noreply.github.com>
2024-03-05 03:42:24 -0500

    Update OBJ Exporter
    - Add missing check

--- viewers/static/jsm/exporters/OBJExporter.js
+++ viewers/static/jsm/exporters/OBJExporter.js
@@ -65,7 +65,7 @@ class OBJExporter {
 			output += 'o ' + mesh.name + '\n';
 
 			// name of the mesh material
-			if ( mesh.material && mesh.material.name ) {
+			if ( mesh.material && mesh.material.name !== undefined ) {
 
 				if ( mesh.material.name === '' ) {
 
