f07a99676f61a4aaeec2f0c534e4363c1b46be76
GitHubDragonFly <69928501+GitHubDragonFly@users.noreply.github.com>
2024-03-26 17:32:05 -0400

    Update BIM Loader

--- viewers/static/dotbim/loader/module/BIMLoader.js
+++ viewers/static/dotbim/loader/module/BIMLoader.js
@@ -14,6 +14,10 @@ import {
 	Vector3
 } from "three";
 
+import { mergeVertices } from "https://cdn.jsdelivr.net/npm/three@0.162.0/examples/jsm/utils/BufferGeometryUtils.js";
+
+// Based on: https://github.com/ricaun/dotbim.three.js
+
 class BIMLoader extends Loader {
 
 	constructor( manager ) {
@@ -62,6 +66,221 @@ class BIMLoader extends Loader {
 		const mesh_id_keys = {};
 		const bim_meshes = new Group();
 		const scale = new Vector3( 1.0, 1.0, 1.0 );
+
+		function dotbim_Elemments2Meshes( elements, geometries ) {
+
+			return elements.map( element => dotbim_Elemment2Mesh( element, geometries ) );
+
+		}
+
+		function dotbim_Elemment2Mesh( element, geometries ) {
+
+			let { mesh_id, vector, rotation, guid, type, color, face_colors, info } = element;
+
+			let geometry;
+
+			if ( geometries[ mesh_id ] ) {
+
+				geometry = geometries[ mesh_id ].clone();
+
+			} else {
+
+				for ( const geo of geometries ) {
+
+					if ( geo.userData.mesh_id === mesh_id ) {
+
+						geometry = geo.clone();
+						break;
+
+					}
+
+				}
+
+				if ( ! geometry ) {
+
+					throw new Error( 'THREE.BIMLoader: Geometry not found!' );
+
+				}
+
+			}
+
+			let name = ( info && info.Name ) ? info.Name : '';
+
+			geometry.computeVertexNormals();
+
+			let material = new MeshStandardMaterial( {
+
+				name: ( info && info[ 'Material' ] ) ? info[ 'Material' ] : '__DEFAULT',
+				side: DoubleSide,
+				flatShading: false,
+				transparent: true,
+				metalness: 0.4,
+				roughness: 0.6,
+				color: 0xFFFFFF
+
+			} );
+
+			if ( color ) {
+
+				if ( color.r === 0 && color.g === 0 && color.b === 0 && color.a === 0 ) color = null;
+
+			}
+
+			// Support `face_colors` in element
+
+			if ( face_colors && face_colors.length > 0 ) {
+
+				if ( geometry.index ) geometry = geometry.toNonIndexed(); // Remove index to make color work with face
+
+				if ( geometry.hasAttribute( 'color' ) ) geometry.deleteAttribute( 'color' );
+
+				const buffer_colors = createFaceColors( face_colors, 3, 4 * geometry.attributes.position.count );
+				geometry.setAttribute( 'color', new Float32BufferAttribute( buffer_colors, 4 ) );
+
+				material.color.setRGB( 1, 1, 1 );
+				material.opacity = 1.0;
+				material.transparent = true;
+				material.vertexColors = true;
+
+			} else if ( color ) {
+
+				material.color = new Color( color.r / 255.0, color.g / 255.0, color.b / 255.0, SRGBColorSpace );
+				material.opacity = color.a / 255.0;
+				material.transparent = material.opacity < 1.0;
+
+			}
+
+			if ( ! vector ) vector = { x: 0, y: 0, z: 0 };
+			if ( ! rotation ) rotation = { qx: 0, qy: 0, qz: 0, qw: 1 };
+
+			let mesh;
+
+			if ( face_colors && mesh_id_keys[ mesh_id ][ 'face_colors_group' ][ face_colors ] ) {
+
+				let mesh_id_key = mesh_id_keys[ mesh_id ][ 'face_colors_group' ][ face_colors ];
+
+				if ( mesh_id_key[ 'mesh' ] === null ) {
+
+					mesh_id_key[ 'mesh' ] = new InstancedMesh( geometry, material, mesh_id_key.instance_count );
+
+				}
+
+				mesh = mesh_id_key.mesh;
+
+				let pos = new Vector3( vector.x, vector.y, vector.z );
+				let rotq = new Quaternion( rotation.qx, rotation.qy, rotation.qz, rotation.qw );
+
+				let matrix = new Matrix4().compose( pos, rotq, scale );
+
+				mesh.setMatrixAt( mesh_id_key.current_instance, matrix );
+				mesh.instanceMatrix.needsUpdate = true;
+
+				if (name === '') name = 'mesh_' + mesh_id + '_' + mesh.id + '_' + mesh_id_key.current_instance;
+
+				mesh.userData[ mesh_id_key.current_instance ] = { name: name, guid: guid, type: type, info: info || {} };
+
+				mesh_id_key.current_instance++;
+
+			} else { // expected existing 'color'
+
+				let el_color = [ color.r, color.g, color.b, color.a ];
+				let mesh_id_key = mesh_id_keys[ mesh_id ][ 'color_group' ][ el_color ];
+
+				if (mesh_id_key[ 'mesh' ] === null) {
+
+					mesh_id_key[ 'mesh' ] = new InstancedMesh( geometry, material, mesh_id_key.instance_count );
+
+				}
+
+				mesh = mesh_id_key.mesh;
+
+				let pos = new Vector3( vector.x, vector.y, vector.z );
+				let rotq = new Quaternion( rotation.qx, rotation.qy, rotation.qz, rotation.qw );
+
+				let matrix = new Matrix4().compose( pos, rotq, scale );
+
+				mesh.setMatrixAt( mesh_id_key.current_instance, matrix );
+				mesh.instanceMatrix.needsUpdate = true;
+
+				mesh.setColorAt( mesh_id_key.current_instance, material.color );
+				mesh.instanceColor.needsUpdate = true;
+
+				if (name === '') name = 'mesh_' + mesh_id + '_' + mesh.id + '_' + mesh_id_key.current_instance;
+
+				mesh.userData[ mesh_id_key.current_instance ] = { name: name, guid: guid, type: type, info: info || {} };
+
+				mesh_id_key.current_instance++;
+
+			}
+
+			return mesh;
+
+		}
+
+		function dotbim_Meshes2Geometries( meshes ) {
+
+			return meshes.map( mesh => dotbim_Mesh2GeometryColor( mesh ) );
+
+		}
+
+		function dotbim_Mesh2GeometryColor( mesh ) {
+
+			const { mesh_id, coordinates, indices, colors } = mesh;
+
+			let geometry = new BufferGeometry();
+
+			geometry.setAttribute( 'position', new Float32BufferAttribute( coordinates, 3 ) );
+			geometry.computeVertexNormals();
+
+			if ( colors && colors.length > 0 ) {
+
+				const buffer_colors = createFaceColors( colors, 3, 4 * geometry.attributes.position.count );
+				geometry.setAttribute( 'color', new Float32BufferAttribute( buffer_colors, 4 ) );
+
+			} else {
+
+				if ( indices ) {
+
+					geometry.setIndex( indices );
+
+				} else {
+
+					geometry = mergeVertices( geometry );
+
+				}
+
+			}
+
+			geometry.userData[ 'mesh_id' ] = mesh_id;
+
+			return geometry;
+
+		}
+
+		function createFaceColors( color4arrary, repeat = 3, max = 0 ) {
+
+			let colors = [];
+
+			for ( let index = 0, length = color4arrary.length; index < length; index += 4 ) {
+
+				let c1 = color4arrary[ index + 0 ] / 255.0;
+				let c2 = color4arrary[ index + 1 ] / 255.0;
+				let c3 = color4arrary[ index + 2 ] / 255.0;
+				let c4 = color4arrary[ index + 3 ] / 255.0;
+
+				for ( let i = 0; i < repeat; i++ ) { colors.push( c1, c2, c3, c4 ); }
+
+			}
+
+			while ( colors.length < max ) {
+
+				for ( let k = 0; k < repeat; k++ ) { colors.push( colors[ 0 ], colors[ 1 ], colors[ 2 ], colors[ 3 ] ); }
+
+			}
+
+			return colors;
+
+		}
 
 		function dotbim_CreateMeshes( dotbim ) {
 
@@ -136,239 +355,6 @@ class BIMLoader extends Loader {
 			if ( bim_meshes.children.length > 1 ) bim_meshes.rotateX( - Math.PI / 2 );
 
 			return bim_meshes;
-
-		}
-
-		function dotbim_Elemments2Meshes( elements, geometries ) {
-
-			return elements.map( element => dotbim_Elemment2Mesh( element, geometries ) );
-
-		}
-
-		function dotbim_Elemment2Mesh( element, geometries ) {
-
-			let { mesh_id, vector, rotation, guid, type, color, face_colors, info } = element;
-
-			let geometry;
-
-			if ( geometries[ mesh_id ] ) {
-
-				geometry = geometries[ mesh_id ].clone();
-
-			} else {
-
-				for ( const geo of geometries ) {
-
-					if ( geo.userData.mesh_id === mesh_id ) {
-
-						geometry = geo.clone();
-						break;
-
-					}
-
-				}
-
-				if ( ! geometry ) {
-
-					throw new Error( 'THREE.BIMLoader: Geometry not found!' );
-
-				}
-
-			}
-
-			let name = info.Name || '';
-
-			geometry.computeVertexNormals();
-
-			let material = new MeshStandardMaterial( {
-
-				name: info[ 'Material' ] || 'Default Material',
-				side: DoubleSide,
-				flatShading: false,
-				transparent: true,
-				metalness: 0.5,
-				roughness: 0.5,
-				color: 0xFFFFFF
-
-			} );
-
-			if ( color ) {
-
-				if ( color.r === 0 && color.g === 0 && color.b === 0 && color.a === 0 ) color = null;
-
-			}
-
-			// Support `face_colors` in element
-
-			if ( face_colors ) {
-
-				geometry = geometry.clone();
-
-				let colors = createFaceColors( face_colors );
-
-				geometry.setAttribute( 'color', new Float32BufferAttribute( colors, 4 ) );
-
-			} else if ( color )	{
-
-				geometry = geometry.clone();
-
-				geometry.deleteAttribute( 'color' ); // Remove default color in the geometry
-
-				material.color = convertTHREEColorRGB( color.r, color.g, color.b );
-				material.opacity = convertColorAlpha( color.a );
-				material.transparent = material.opacity < 1.0;
-
-			}
-
-			// Force the use of geometry color if exists ('colors')
-
-			if ( geometry.getAttribute( 'color' ) ) {
-
-				material.color.setRGB( 1, 1, 1 );
-				material.opacity = 1.0;
-				material.transparent = true;
-				material.vertexColors = true;
-
-			}
-
-			if ( ! vector ) vector = { x: 0, y: 0, z: 0 };
-			if ( ! rotation ) rotation = { qx: 0, qy: 0, qz: 0, qw: 1 };
-
-			let mesh;
-
-			if ( face_colors && mesh_id_keys[ mesh_id ][ 'face_colors_group' ][ face_colors ] ) {
-
-				let mesh_id_key = mesh_id_keys[ mesh_id ][ 'face_colors_group' ][ face_colors ];
-
-				if ( mesh_id_key[ 'mesh' ] === null ) {
-
-					mesh_id_key[ 'mesh' ] = new InstancedMesh( geometry, material, mesh_id_key.instance_count );
-
-				}
-
-				mesh = mesh_id_key.mesh;
-
-				let pos = new Vector3( vector.x, vector.y, vector.z );
-				let rotq = new Quaternion( rotation.qx, rotation.qy, rotation.qz, rotation.qw );
-
-				let matrix = new Matrix4().compose( pos, rotq, scale );
-
-				mesh.setMatrixAt( mesh_id_key.current_instance, matrix );
-				mesh.instanceMatrix.needsUpdate = true;
-
-				if (name === '') name = 'mesh_' + mesh_id + '_' + mesh.id + '_' + mesh_id_key.current_instance;
-
-				mesh.userData[ mesh_id_key.current_instance ] = { name: name, guid: guid || {}, type: type || {}, info: info || {} };
-
-				mesh_id_key.current_instance++;
-
-			} else { // expected existing 'color'
-
-				let el_color = [ color.r, color.g, color.b, color.a ];
-				let mesh_id_key = mesh_id_keys[ mesh_id ][ 'color_group' ][ el_color ];
-
-				if (mesh_id_key[ 'mesh' ] === null) {
-
-					mesh_id_key[ 'mesh' ] = new InstancedMesh( geometry, material, mesh_id_key.instance_count );
-
-				}
-
-				mesh = mesh_id_key.mesh;
-
-				let pos = new Vector3( vector.x, vector.y, vector.z );
-				let rotq = new Quaternion( rotation.qx, rotation.qy, rotation.qz, rotation.qw );
-
-				let matrix = new Matrix4().compose( pos, rotq, scale );
-
-				mesh.setMatrixAt( mesh_id_key.current_instance, matrix );
-				mesh.instanceMatrix.needsUpdate = true;
-
-				mesh.setColorAt( mesh_id_key.current_instance, material.color );
-				mesh.instanceColor.needsUpdate = true;
-
-				if (name === '') name = 'mesh_' + mesh_id + '_' + mesh.id + '_' + mesh_id_key.current_instance;
-
-				mesh.userData[ mesh_id_key.current_instance ] = { name: name, guid: guid || {}, type: type || {}, info: info || {} };
-
-				mesh_id_key.current_instance++;
-
-			}
-
-			return mesh;
-
-		}
-
-		function dotbim_Meshes2Geometries( meshes ) {
-
-			return meshes.map( mesh => dotbim_Mesh2GeometryColor( mesh ) );
-
-		}
-
-		function dotbim_Mesh2GeometryColor( mesh ) {
-
-			const { mesh_id, coordinates, indices, colors } = mesh;
-
-			let geometry = new BufferGeometry();
-
-			geometry.setIndex( indices );
-			geometry.setAttribute( 'position', new Float32BufferAttribute( coordinates, 3 ) );
-			geometry = geometry.toNonIndexed(); // Use this to remove Index and make color work with face
-
-			if ( colors ) {
-
-				buffer_colors = createFaceColors( colors, 3, 4 * indices.length );
-				geometry.setAttribute( 'color', new Float32BufferAttribute( buffer_colors, 4 ) );
-
-			}
-
-			geometry.computeVertexNormals();
-
-			geometry.userData[ 'mesh_id' ] = mesh_id;
-
-			return geometry;
-
-		}
-
-		function createFaceColors( color4arrary, repeat = 3, max = 0 ) {
-
-			let colors = [];
-
-			for ( let index = 0, length = color4arrary.length; index < length; index += 4 ) {
-
-				let c1 = color4arrary[ index + 0 ];
-				let c2 = color4arrary[ index + 1 ];
-				let c3 = color4arrary[ index + 2 ];
-
-				let c4 = convertColorAlpha( color4arrary[ index + 3 ] );
-
-				let color = convertTHREEColorRGB( c1, c2, c3 );
-
-				for ( let i = 0; i < repeat; i++ ) colors.push( color.r, color.g, color.b, c4 );
-
-			}
-
-			while ( colors.length < max ) {
-
-				for ( let i = 0; i < repeat; i++ ) colors.push( colors[ 0 ], colors[ 1 ], colors[ 2 ], colors[ 3 ] );
-
-			}
-
-			return colors;
-
-		}
-
-		function convertTHREEColorRGB( r, g, b ) {
-
-			let new_color = new Color( r / 255.0, g / 255.0, b / 255.0 );
-			new_color.setStyle( new_color.getStyle(), SRGBColorSpace );
-
-			return new_color;
-
-		}
-
-		function convertColorAlpha( alpha )	{
-
-			return alpha / 255.0;
 
 		}
 
