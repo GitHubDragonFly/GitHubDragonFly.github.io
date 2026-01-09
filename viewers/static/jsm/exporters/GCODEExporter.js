e6b5503bd5e38320c0ef2394e7d6330d4287bd8b
GitHubDragonFly <69928501+GitHubDragonFly@users.noreply.github.com>
2026-01-09 01:35:30 -0500

    Update GCODE Exporter

--- viewers/static/jsm/exporters/GCODEExporter.js
+++ viewers/static/jsm/exporters/GCODEExporter.js
@@ -1,5 +1,5 @@
 import { DefaultLoadingManager } from "three";
-import Polyslice from "https://cdn.jsdelivr.net/npm/@jgphilpott/polyslice@25.12.9/dist/index.browser.esm.js";
+import Polyslice from "https://cdn.jsdelivr.net/npm/@jgphilpott/polyslice@26.1.0/dist/index.browser.esm.js";
 
 /**
 *
